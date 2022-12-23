520601b30804ba60a616ad4d834542ef2eee70d1
TahaFakhran <tahafakhran@gmail.com>
2022-12-23 12:33:08 +0100

    Test Main

---
+++ src/main/java/com/example/aiproject/MainTest.java
@@ -0,0 +1,23 @@
+package com.example.aiproject;
+
+public class MainTest {
+    public static void main(String[] args) {
+        int n = 8;
+        Integer[] list = {5, 0, 2, 8, 6, 4, 7, 3, 1};
+        int size = ((int) Math.sqrt(n) + 1);
+
+
+        Integer[][] numbers = new Integer[size][size];
+        int counter = 0;
+        for (int r = 0; r < size; r++) {
+            for (int c = 0; c < size; c++) {
+                numbers[r][c] = list[counter++];
+            }
+        }
+        Board board = new Board(numbers);
+
+    }
+
+}
+
+
